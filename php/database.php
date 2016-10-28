ed57cb0239f15837778d4e444adbb2aaf4892f30
mehmet <mehmet-han@yandex.com>
2016-10-28 18:19:12 +0300

    database.php improvements

--- php/database.php
+++ php/database.php
@@ -80,6 +80,17 @@ class Database{
 		}
 	}	
 
+	function query($sql){
+		try{
+			$stmt = $this->conn->prepare($sql);
+			$stmt->execute();
+			return $stmt;
+		}catch(PDOException $e){
+			echo "Sql Statement Failed: ". $e->getMessage();
+			return false;
+		}
+	}
+
 
 	function insert($table, $valuesArray){
 		$placeholders = [];
@@ -94,8 +105,55 @@ class Database{
 
 			return $this->conn->lastInsertId();
 		}catch(PDOException $e){
-			die("Insert Statement Failed: ". $e->getMessage());
+			echo "Insert Statement Failed: ". $e->getMessage();
+			return false;
 		}
+	}
+
+	/* Example usages of select function:
+		
+		$result = $db->select("users", "name, age");
+		$result = $db->select("users", "*", [ ["name", "=", "john"], ["age", ">", 21] ], "or", 10);
+		$result = $db->select("users", ["id", "name", "age"], [ ["age", ">", 21] ]);
+
+	*/
+
+	//Order by should be implemented
+	function select($table, $columns = "*", $conditions = [], $operator = "AND", $limit = 0){
+		
+		$columns = empty($columns) ? "*" : $columns;
+		$columns = is_array($columns) ? implode(',', $columns) : $columns;
+
+		$limit = is_int($limit) && $limit > 0 ? $limit : 0;
+
+		$operator = in_array(strtolower($operator), ["or", "and"]) ? strtoupper($operator) : "AND";
+
+		$placeholders = [];
+		$condition_values = [];
+		foreach ($conditions as $condition) {
+			$placeholders[] = $condition[0].$condition[1].":".$condition[0];
+			$condition_values[$condition[0]] = $condition[2]; 
+		}
+
+		$sql = "SELECT ". $columns ." FROM ". $table;
+
+		if(!empty($conditions)){
+			$sql .= " WHERE ". implode(' '.$operator.' ', $placeholders);
+		}
+
+		if($limit){
+			$sql .= " LIMIT ".$limit;
+		}
+
+		try{
+			$stmt = $this->conn->prepare($sql);
+			$stmt->execute($condition_values);
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}catch(PDOException $e){
+			echo "Select Statement Failed: ". $e->getMessage();
+			return false;
+		}
+
 	}
 
 }
