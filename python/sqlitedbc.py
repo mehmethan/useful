fd43601e09b4a9a07b0e68866e09675202f81e90
mehmet <mehmet-han@yandex.com>
2016-11-19 19:23:30 +0300

    sqlitedbc improvements

--- python/sqlitedbc.py
+++ python/sqlitedbc.py
@@ -48,8 +48,8 @@ class SqliteDBC:
 	def eq(self, key_value = {}):
 		try:
 			key = key_value.keys()[0]
-			self.where_dict[key] = key_value[key]
-			self.where = self.where + " {} = :{} ".format(key, key)
+			self.where_dict['w_'+key] = key_value[key]
+			self.where = self.where + " {} = :w_{} ".format(key, key)
 			return self
 		except Exception as e:
 			raise e
@@ -59,8 +59,8 @@ class SqliteDBC:
 	def neq(self, key_value = {}):
 		try:
 			key = key_value.keys()[0]
-			self.where_dict[key] = key_value[key]
-			self.where = self.where + " {} != :{} ".format(key, key)
+			self.where_dict['w_'+key] = key_value[key]
+			self.where = self.where + " {} != :w_{} ".format(key, key)
 			return self
 		except Exception as e:
 			raise e
@@ -69,8 +69,8 @@ class SqliteDBC:
 	def gt(self, key_value = {}):
 		try:
 			key = key_value.keys()[0]
-			self.where_dict[key] = key_value[key]
-			self.where = self.where + " {} > :{} ".format(key, key)
+			self.where_dict['w_'+key] = key_value[key]
+			self.where = self.where + " {} > :w_{} ".format(key, key)
 			return self
 		except Exception as e:
 			raise e
@@ -79,8 +79,8 @@ class SqliteDBC:
 	def gte(self, key_value = {}):
 		try:
 			key = key_value.keys()[0]
-			self.where_dict[key] = key_value[key]
-			self.where = self.where + " {} >= :{} ".format(key, key)
+			self.where_dict['w_'+key] = key_value[key]
+			self.where = self.where + " {} >= :w_{} ".format(key, key)
 			return self
 		except Exception as e:
 			raise e
@@ -89,8 +89,8 @@ class SqliteDBC:
 	def lt(self, key_value = {}):
 		try:
 			key = key_value.keys()[0]
-			self.where_dict[key] = key_value[key]
-			self.where = self.where + " {} < :{} ".format(key, key)
+			self.where_dict['w_'+key] = key_value[key]
+			self.where = self.where + " {} < :w_{} ".format(key, key)
 			return self
 		except Exception as e:
 			raise e
@@ -99,8 +99,8 @@ class SqliteDBC:
 	def lte(self, key_value = {}):
 		try:
 			key = key_value.keys()[0]
-			self.where_dict[key] = key_value[key]
-			self.where = self.where + " {} <= :{} ".format(key, key)
+			self.where_dict['w_'+key] = key_value[key]
+			self.where = self.where + " {} <= :w_{} ".format(key, key)
 			return self
 		except Exception as e:
 			raise e
@@ -171,12 +171,74 @@ class SqliteDBC:
 		return result
 
 
-	def update(self):
-		pass
+	def update(self, values = {}):
+		placeholders = [key + ' = :'+key for key in values.keys()]
+		sql = "UPDATE {} SET {}".format(self.table_name, ','.join(placeholders))
+
+		if self.where != "":
+			sql = sql + " WHERE {} ".format(self.where)
+
+		if int(self.limit_count) > 0:
+			sql = sql + " LIMIT {}".format(self.limit_count)
+
+		
+
+		try:
+			#merge two dictionaries
+			values.update(self.where_dict)
+
+			cursor = self.conn.cursor()
+			cursor.execute(sql, values)
+			self.conn.commit()
+			row_count = cursor.rowcount
+			cursor.close()
+		except Exception as e:
+			raise e
+
+		self.table_name = ""
+		self.where = ""
+		self.where_dict = {}
+		self.limit_count = 0
+
+		return row_count
+		
+
 
 	def delete(self):
-		pass
+		sql = "DELETE FROM {} ".format(self.table_name)
+
+		if self.where != "":
+			sql = sql + " WHERE {}".format(self.where)
+
+		if int(self.limit_count) > 0:
+			sql = sql + " LIMIT {}".format(self.limit_count)
+
+		try:
+			cursor = self.conn.cursor()
+			cursor.execute(sql, self.where_dict)
+			row_count = cursor.rowcount
+			self.conn.commit()
+			cursor.close()
+
+		except Exception as e:
+			raise e
+
+		self.table_name = ""
+		self.where = ""
+		self.where_dict = {}
+		self.limit_count = 0
+
+		return row_count 
 
 
 	def execute(self, sql):
-		pass
+		try:
+			cursor = self.conn.cursor()
+			cursor.execute(sql)
+			self.conn.commit()
+
+			return cursor
+
+		except Exception as e:
+			raise e
+
